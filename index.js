7e2866ae0081247e1984f28eb83190f972e6af0e
Steve Edson <steve3d50n@gmail.com>
2020-09-23 18:51:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const getCacheDirs = (constants) => [
   constants.PUBLISH_DIR,
-  path.normalize(`${constants.PUBLISH_DIR}/../.cache`),
+  path.normalize(`${constants.PUBLISH_DIR}/../.nuxt`),
 ];
 
 module.exports = {
@@ -10,25 +10,25 @@ module.exports = {
     // print a helpful message if the publish dir is misconfigured
     if (process.cwd() === constants.PUBLISH_DIR) {
       utils.build.failBuild(
-        `Gridsome sites must publish the dist directory, but your site’s publish directory is set to “${constants.PUBLISH_DIR}”. Please set your publish directory to your Gridsome site’s dist directory.`,
+        `Nuxt sites must publish the dist directory, but your site’s publish directory is set to “${constants.PUBLISH_DIR}”. Please set your publish directory to your Nuxt site’s dist directory.`,
       );
     }
 
     const cacheDirs = getCacheDirs(constants);
 
     if (await utils.cache.restore(cacheDirs)) {
-      console.log('Found a Gridsome cache. We’re about to go FAST. ⚡️');
+      console.log('Found a Nuxt cache. We’re about to go FAST. ⚡️');
     } else {
-      console.log('No Gridsome cache found. Building fresh.');
+      console.log('No Nuxt cache found. Building fresh.');
     }
   },
   async onPostBuild({ constants, utils }) {
     const cacheDirs = getCacheDirs(constants);
 
     if (await utils.cache.save(cacheDirs)) {
-      console.log('Stored the Gridsome cache to speed up future builds.');
+      console.log('Stored the Nuxt cache to speed up future builds.');
     } else {
-      console.log('No Gridsome build found.');
+      console.log('No Nuxt build found.');
     }
   },
 };
